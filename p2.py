49331794f0a137146cf1518b9063827523f0e09e
Kishan Ramesh <55047167+Kishan1082@users.noreply.github.com>
2023-10-06 09:16:38 -0400

    Add files via upload

--- p2.py
+++ p2.py
@@ -11,11 +11,11 @@ def stair_recurrence(points): #Recurrence function to divide and conquer
     if(len(points)==1):
         return points
     
-    sorted_pts = sorted(points, key=lambda p:(p[1],p[0]), reverse=True)
+    sorted_pts = sorted(points, key=lambda p:(p[1],p[0]), reverse=True) #sorting
     
-    mid = len(sorted_pts)//2
+    mid = len(sorted_pts)//2   
 
-    left_half = stair_recurrence(sorted_pts[:mid])
+    left_half = stair_recurrence(sorted_pts[:mid]) #recurrence calls
     right_half = stair_recurrence(sorted_pts[mid:])
 
     return stair_merge(left_half, right_half)
@@ -32,17 +32,17 @@ def stair_merge(staircase1, staircase2): #function to merge
             merged_staircase.append(staircase2[j])
             j+=1
     
-    merged_staircase.extend(staircase1[i:])
+    merged_staircase.extend(staircase1[i:]) #appending the rest
     merged_staircase.extend(staircase2[j:])
 
     return merged_staircase
     
 
-n = int(input("Enter the value of n:"))
+n = int(input("Enter the value of n:")) #taking user input
 points = ran_gen(n)
 
-if(points == None):
+if(points == None): #if input is invalid
     print("Invalid input")
 else:
-    result = stair_recurrence(points)
-    print(result)
+    result = stair_recurrence(points) #calling the function
+    print(result) #final result
